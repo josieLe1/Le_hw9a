f04b59e904eb43e8a84554e4fcb684dcba6fc452
John Doe <johndoe@email.com>
2023-04-05 00:15:19 -0700

    final

---
+++ ex1/circle.js
@@ -0,0 +1,14 @@
+
+
+const circumference = (radius) => {
+  return 2 * 3.14 * radius;
+};
+
+const area = (radius) => {
+  return 3.14 * radius ** 2;
+};
+
+module.exports = {
+  circumference,
+  area
+};
---
+++ ex1/index.js
@@ -0,0 +1,6 @@
+
+
+const circle = require('./circle.js');
+
+console.log(`The circumference of a circle with radius 3 is ${circle.circumference(3)}.`);
+console.log(`Its area is ${circle.area(3)}.`);
